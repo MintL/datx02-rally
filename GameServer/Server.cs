817720cc67c6e922ab86e6ad78491055f65f13fa
Johannes Keinestam <johannes.keinestam@gmail.com>
2013-02-03 19:48:38 +0100

    Simple server using Lidgren that can receive data from clients and react accordingly

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Lidgren.Network;
+using System.Net;
 
 namespace GameServer
 {
@@ -11,19 +12,75 @@ namespace GameServer
         string ServerName;
         readonly int MaxPlayers;
         ServerPlayer[] Players;
+        readonly int Port;
+        NetServer serverThread;
 
         public Server()
         {
             ServerName = "Test server";
             MaxPlayers = 4;
             Players = new ServerPlayer[4];
+            Port = 14242; 
 
             Start();
+        }
+
+        private void Run()
+        {
+            NetIncomingMessage msg;
+            while ((msg = serverThread.ReadMessage()) != null)
+            {
+                HandleIncomingPacket(msg);
+            }
+        }
+
+        private void HandleIncomingPacket(NetIncomingMessage msg)
+        {
+            switch (msg.MessageType)
+            {
+                case NetIncomingMessageType.DiscoveryRequest:
+                    Console.WriteLine("Server discovered by new player " + msg.SenderEndPoint.Address);
+                    break;
+                case NetIncomingMessageType.StatusChanged:
+                    NetConnectionStatus status = (NetConnectionStatus)msg.ReadByte();
+                    switch (status)
+                    {
+                        case NetConnectionStatus.Connected:
+                            PlayerConnected(msg.SenderEndPoint);
+                            break;
+                        case NetConnectionStatus.Disconnected:
+                            PlayerDisconnected(msg.SenderEndPoint);
+                            break;
+                        default:
+                            Console.WriteLine("Received unhandled status message " + status + " from player " + msg.SenderEndPoint.Address);
+                            break;
+                    }
+                    break;
+                case NetIncomingMessageType.Data:
+                    ProcessDataMessage(msg);
+                    break;
+                default:
+                    Console.WriteLine("Received unhandled message type '" + msg.MessageType + "' from player " + msg.SenderEndPoint.Address);
+                    break;
+            }
+        }
+
+        private void ProcessDataMessage(NetIncomingMessage msg)
+        {
+            Console.WriteLine("Received data from player " + msg.SenderEndPoint.Address + ": " + msg.Data);
         }
 
         public void Start()
         {
             Console.WriteLine("Started server: "+ServerName+".");
+
+            NetPeerConfiguration config = new NetPeerConfiguration("DATX02");
+            config.Port = Port;
+
+            serverThread = new NetServer(config);
+            serverThread.Start();
+
+            Run();
         }
 
         public void Shutdown()
@@ -31,12 +88,14 @@ namespace GameServer
             Console.WriteLine("Shutting down server...");
         }
 
-        public void PlayerConnected()
+        public void PlayerConnected(IPEndPoint playerIP)
         {
-            // TODO
+            serverThread.SendDiscoveryResponse(null, playerIP);
+
+
         }
 
-        public void PlayerDisconnected()
+        public void PlayerDisconnected(IPEndPoint playerIP)
         {
             // TODO
         }
