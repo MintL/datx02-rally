396df0c90a7e0086ac6e893d77024c47dcbfee1e
Johannes Keinestam <johannes.keinestam@gmail.com>
2013-02-07 17:11:04 +0100

    Added missing file.

---
+++ datx02-rally/datx02-rally/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace datx02_rally
+{
+    class Player : GameComponent
+    {
+        Vector3 Position;
+
+        public Player(Game1 game) : base(game)
+        {
+            // TODO
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Gets 
+        /// </summary>
+        /// <returns></returns>
+        private Vector3 GetPosition(GameTime gameTime) 
+        {
+            return new Vector3();
+        }
+    }
+}
