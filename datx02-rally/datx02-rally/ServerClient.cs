f64c1b758409c7f5f8bc4addd1efea227ce1b989
Johannes Keinestam <johannes.keinestam@gmail.com>
2013-02-07 13:28:36 +0100

    Added ServerClient class, with simple logic representing the local server client.

---
+++ datx02-rally/datx02-rally/ServerClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Lidgren.Network;
+using System.Net;
+
+namespace datx02_rally
+{
+    class ServerClient
+    {
+        NetClient client;
+        readonly int PORT = 14242;
+
+        public ServerClient()
+        {
+            NetPeerConfiguration config = new NetPeerConfiguration("DATX02");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+
+            client = new NetClient(config);
+            client.Start();
+        }
+
+        public void Connect(IPAddress IP) {
+            client.Connect(new IPEndPoint(IP, PORT));
+        }
+    }
+}
