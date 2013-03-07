0c332664db6902c5922ade1d8b0d6f7a840699d0
Riesz <fredrik.tiwaz@gmail.com>
2013-03-07 16:06:51 +0100

    Started working on sound, creating the (probably) needed managers and such. !!Don't try using anything!! There are currently no supporting sound libraries

---
+++ datx02-rally/datx02-rally/Sound/AudioEngineManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+
+namespace datx02_rally.Sound
+{
+    public class AudioEngineManager
+    {
+        public AudioEngine audioEngine;
+        public SoundBank soundBank;
+        public WaveBank waveBank;
+
+        private static AudioEngineManager instance;
+
+
+        public static AudioEngineManager GetInstance()
+        {
+            if (instance == null) instance = new AudioEngineManager();
+            return instance;
+        }
+
+        private AudioEngineManager() 
+        {
+            //create and use appropriate XACT objects
+            audioEngine = new AudioEngine("");
+            soundBank = new SoundBank(audioEngine, "");
+            waveBank = new WaveBank(audioEngine, "");
+        }
+
+
+        /// <summary>
+        /// Plays a single sound once
+        /// </summary>
+        /// <param name="cueName">The sound to be played</param>
+        public static void PlaySound(string cueName)
+        {
+            AudioEngineManager.GetInstance().soundBank.PlayCue(cueName);
+        }
+
+
+        /// <summary>
+        /// Call once per update cycle to update what sounds to play
+        /// </summary>
+        public void Update() 
+        {
+            audioEngine.Update();
+        }
+
+
+    }
+}
---
+++ datx02-rally/datx02-rally/Sound/EnvironmentSoundManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace datx02_rally.Sound
+{
+    public class CueEmitter
+    {
+        protected AudioEmitter audioEmitter;
+        protected Cue cue;
+
+        public CueEmitter(Cue sound, Vector3 position)
+        {
+            cue = sound;
+
+            audioEmitter = new AudioEmitter();
+            audioEmitter.Position = position;
+            audioEmitter.Velocity = Vector3.Zero;
+        }
+
+        public AudioEmitter GetEmitter()
+        {
+            return audioEmitter;
+        }
+
+        public Cue GetCue()
+        {
+            return cue;
+        }
+    }
+
+    public class EnvironmentSoundManager
+    {
+        AudioListener player;
+
+        AudioEngineManager AEM;
+
+        List<CueEmitter> environmentSounds;
+
+
+        //Only one listener, several emitters
+        //Store emitters in a list?
+        //Store listener as a single variable in the class
+        //Update receives emitters current position and velocity
+        //
+
+        public EnvironmentSoundManager(Vector3 position, Vector3 velocity) 
+        {
+            player = new AudioListener();
+            player.Position = position;
+            player.Velocity = velocity;
+
+            environmentSounds = new List<CueEmitter>();
+
+            AEM = AudioEngineManager.GetInstance();
+        }
+
+        public void AddNewSound(string cueName, Vector3 position)
+        {
+            Cue cue = AEM.soundBank.GetCue(cueName);
+
+            environmentSounds.Add(new CueEmitter(cue, position));
+        }
+
+        public void Update(Vector3 position, Vector3 velocity)
+        {
+            player.Position = position;
+            player.Velocity = velocity;
+
+            foreach (CueEmitter sound in environmentSounds) 
+            {
+                sound.GetCue().Apply3D(player, sound.GetEmitter());
+
+                //add dopplereffect and possibly other things
+            }
+        }
+
+    }
+}
