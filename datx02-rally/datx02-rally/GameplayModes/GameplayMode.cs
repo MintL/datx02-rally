a91ecac2b51ccd724ddcea639ad315bb3d9b6d31
Johannes Keinestam <johannes.keinestam@gmail.com>
2013-04-08 22:40:12 +0200

    I possibly have something code-wide sane regarding gameplay states now.

--- datx02-rally/datx02-rally/GameplayModes/GameplayMode.cs
+++ datx02-rally/datx02-rally/GameplayModes/GameplayMode.cs
@@ -1,9 +1,69 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using datx02_rally.EventTrigger;
 
 namespace datx02_rally
 {
-    interface GameplayMode
+    abstract class GameplayMode
     {
-        
+        protected List<GameModeState> states;
+        private int currentState;
+        public bool GameOver { private set; get; }
+
+        public GameplayMode()
+        {
+            GameOver = false;
+            currentState = 0;
+            Initialize();
+        }
+
+        /// <summary>
+        /// For initializing the states list
+        /// </summary>
+        public abstract void Initialize();
+
+        public void Update(GameTime gameTime)
+        {
+            GameModeState current = states[currentState];
+            current.Update(gameTime);
+            if (current.IsStateFinished())
+                currentState++;
+            if (currentState > states.Count - 1)
+                GameOver = true;
+        }
+
+    }
+
+
+    class GameModeState 
+    {
+        public Dictionary<PlayerWrapperTrigger, bool> Triggers = new Dictionary<PlayerWrapperTrigger, bool>();
+
+        public GameModeState(PlayerWrapperTrigger[] triggers) 
+        {
+            foreach (var trigger in triggers)
+		        Triggers.Add(trigger, false);
+        }
+
+        public void Update(GameTime gameTime) 
+        {
+            foreach (var triggerPair in Triggers)
+            {
+                triggerPair.Key.Update(gameTime);
+                if (!triggerPair.Value)
+                    Triggers[triggerPair.Key] = triggerPair.Key.Active;
+            }
+        }
+
+        public bool IsStateFinished()
+        {
+            foreach (bool wasTriggered in Triggers.Values)
+            {
+                if (!wasTriggered) 
+                    return false;
+            }
+            return true;
+        }
+
     }
 }
